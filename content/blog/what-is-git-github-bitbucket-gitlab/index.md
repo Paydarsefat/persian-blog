633ba270f8631de30d31c361ba32e2b00219276b
Ehsan Gazar <ehsan.gazar@beteasy.com.au>
2020-05-17 12:16:27 +1000

    added git github bitbucket post

---
+++ content/blog/what-is-git-github-bitbucket-gitlab/index.md
@@ -0,0 +1,8 @@
+---
+title: معرفی Git و سایت‌های BitBucket, Github و GitLab
+date: "2020-05-17"
+description: "معرفی Git و سایت‌های BitBucket, Github و GitLab"
+videoSourceURL: https://www.youtube.com/embed/JPGH0KwVl8M
+videoTitle: "معرفی Git و سایت‌های BitBucket, Github و GitLab"
+cover: cover.jpg
+---
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,7 +2,7 @@ import React from "react"
 const Video = ({ videoSourceURL, videoTitle, ...props }) => (
   <div className="Video">
     <iframe 
-      src="https://www.youtube.com/embed/zCl2sU03tcc" 
+      src={videoSourceURL} 
       frameborder="0" 
       title={videoTitle}
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
