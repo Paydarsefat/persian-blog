0275f1cea394af8f8b18fdc2f332f064241b4165
Ehsan Gazar <me@ehsangazar.com>
2020-05-19 11:02:34 +1000

    defining global window for SSR

--- src/utils/window.js
+++ src/utils/window.js
@@ -2,10 +2,6 @@ var win;
 
 if (typeof window !== "undefined") {
     win = window;
-} else if (typeof global !== "undefined") {
-    win = global;
-} else if (typeof self !== "undefined") {
-    win = self;
 } else {
     win = {
         location: {
