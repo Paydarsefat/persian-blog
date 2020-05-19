33a60bf697d3671a98a2b7f7586db91df37c0733
Ehsan Gazar <me@ehsangazar.com>
2020-05-19 10:51:09 +1000

    defining global window for SSR

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
+import WINDOW from '../../utils/window'
 
 const Posts = ({ posts }) => {
-  const urlParams = new URLSearchParams(window.location.search);
+  const urlParams = new URLSearchParams(WINDOW.location.search);
   const [chosenCategory, setChosenCategory] = useState(urlParams.get('category'))
   const changeSearchParams = (categoryName) => {
     if (categoryName === 'nothing') {
-      window.history.pushState('page2','All','/');
+      WINDOW.history.pushState('page2','All','/');
       setChosenCategory('')
       return
     }
-    window.history.pushState('page2', categoryName, `?category=${categoryName}`);
+    WINDOW.history.pushState('page2', categoryName, `?category=${categoryName}`);
     setChosenCategory(categoryName)
   }
 
---
+++ src/utils/window.js
@@ -0,0 +1,20 @@
+var win;
+
+if (typeof window !== "undefined") {
+    win = window;
+} else if (typeof global !== "undefined") {
+    win = global;
+} else if (typeof self !== "undefined") {
+    win = self;
+} else {
+    win = {
+        location: {
+            search: '',
+        },
+        history: {
+            pushState = () => {}
+        }
+    };
+}
+
+module.exports = win;
