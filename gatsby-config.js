e64f5f7dc29e761a7dd403b5659dbc140ce3740a
Ehsan Gazar <me@ehsangazar.com>
2020-07-31 10:49:33 +1000

    Added google analytics try third bottom

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-174171864-1",
-        head: true,
+        head: false,
       },
     },
     {
