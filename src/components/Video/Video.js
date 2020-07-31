69742eec33173f298c0a055c1b5ccc3e370673f4
Ehsan Gazar <me@ehsangazar.com>
2020-07-31 11:01:16 +1000

    deleted the iframe

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,13 +1,5 @@
 import React from "react"
 const Video = ({ videoSourceURL, videoTitle, ...props }) => (
-  <div className="Video">
-    <iframe 
-      src={videoSourceURL} 
-      frameborder="0" 
-      title={videoTitle}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-      allowfullscreen>
-    </iframe>
-  </div>
+  <div className="Video">{videoSourceURL}</div>
 )
 export default Video
