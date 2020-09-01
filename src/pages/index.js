08e2a5abcf8fc7c050976b7790ef2b6e8fd3a2fb
Ehsan Gazar <me@ehsangazar.com>
2020-09-02 00:02:49 +1000

    fix pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,61 +7,16 @@ import { Button } from 'react-bootstrap'
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
-  const [posts, setPosts] = useState(data.allMarkdownRemark.edges)
+  const [posts, setPosts] = useState(data.allMarkdownRemark.edges.slice(0, 12))
   const [skip, setSkip] = useState(12)
   const [more, setMore] = useState(true)
 
-  const handleShowMore = async () => {
-    let newPosts = await fetch(`${location.origin}/___graphql`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        query: `{
-          site {
-            siteMetadata {
-              title
-              author {
-                summary
-              }
-            }
-          }
-          allMarkdownRemark(
-            sort: { fields: [frontmatter___date], order: DESC }
-            limit: 12
-            skip: ${skip}
-          ) {
-            edges {
-              node {
-                excerpt
-                fields {
-                  slug
-                }
-                frontmatter {
-                  date(formatString: "MMMM DD, YYYY")
-                  title
-                  description
-                  cover {
-                    childImageSharp {
-                      id
-                      fluid {
-                        src
-                      }
-                    }
-                  }
-                  category
-                }
-              }
-            }
-          }
-        }`,
-      }),
-    })
+  const handleShowMore = () => {
+    const newSkip = skip + 12
 
-    newPosts = await newPosts.json()
-
-    setSkip(skip + 12)
-    setPosts([...posts, ...newPosts.data.allMarkdownRemark.edges])
-    if (newPosts.data.allMarkdownRemark.edges.length < 12) {
+    setSkip(newSkip)
+    setPosts(data.allMarkdownRemark.edges.slice(0, newSkip))
+    if (data.allMarkdownRemark.edges.length < newSkip) {
       setMore(false)
     }
   }
@@ -70,7 +25,7 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle} page="homepage">
       <SEO title={data.site.siteMetadata.author.summary} />
       <Posts posts={posts} />
-      {false && (
+      {more && (
         <div className="loadMore">
           <Button variant="secondary" onClick={handleShowMore}>
             مطالب بیشتر
