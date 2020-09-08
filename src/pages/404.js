9f9f1423ba700a43d8469f20b0d07e5cbb53bd8c
Ehsan Gazar <me@ehsangazar.com>
2020-09-08 22:52:34 +1000

    change 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,26 @@
-import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/Layout/Layout"
-import SEO from "../components/SEO/SEO"
-import "../app/assets/css/elegant.css"
-import "../app/assets/css/custom_bootstrap.css"
-import "../app/assets/css/slick.css"
-import "../app/assets/css/plyr.min.css"
-import "../app/assets/css/style.css"
-import "./index.css"
-import notFoundProfile from "./404.png"
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/Layout/Layout'
+import SEO from '../components/SEO/SEO'
+import '../app/assets/css/elegant.css'
+import '../app/assets/css/custom_bootstrap.css'
+import '../app/assets/css/slick.css'
+import '../app/assets/css/plyr.min.css'
+import '../app/assets/css/style.css'
+import './index.css'
+import notFoundProfile from './404.png'
 
 const NotFoundPage = ({ location }) => {
   return (
-    <Layout location={location} title={"۴۰۴"}>
-      <SEO title={"گم‌شده اید"} />
+    <Layout location={location} title={'۴۰۴'}>
+      <SEO title={'گم‌شده اید'} />
       {/* <Banner /> */}
-      <div class="no-pd" id="content">
+      <div id="content">
         <div class="container">
           <div class="breadcrumb">
             <ul>
               <li>
-                <Link to={"/"}>
+                <Link to={'/'}>
                   <i class="fas fa-home"></i>خانه
                 </Link>
               </li>
@@ -36,16 +36,32 @@ const NotFoundPage = ({ location }) => {
                 <div class="col-8 content">
                   <h1>گم شده‌اید؟</h1>
                   <br />
+                  <p>به نظر دنبال صفحه‌ای می‌گردید که وجود ندارد! حال جرا</p>
+                  <ul>
+                    <li>ممکنه صفحه بوده! ولی دیگه نیست، که عجیبه و بعیده</li>
+                    <li>
+                      ممکنه صفحه بوده، الان موفت نیست؟ که اونم باز با Gatsby
+                      بعیده و غریبه
+                    </li>
+                    <li>
+                      من میگم، صفحه از اول نبوده، پس شما چرا اینجایین؟ شاید یکی
+                      شما رو گمراه کرده؟
+                    </li>
+                    <li>
+                      شایدم خودت از قصد گم شدی؟ پیش میاد، ولی خبری نیست اینجا
+                      خلاصه، امیدوارم پیدا شی
+                    </li>
+                  </ul>
+                  <br />
                   <p>
-                    خب به نظر میاد دنبال چیزی میگردید که وجود ندارد؟
-                    <br />
-                    شاید هدف خودتان را فراموش کرده‌اید؟
-                    <br />
-                    <br />
-                    هر آنچه هست احتمالا اگر تنها روی گزینه خانه‌ بالا سمت چپ کلیک کنید پیدا خواهید شد.
+                    حالا که Debugging کامل انجام شد، وقتشه برگردیم سرجای
+                    اصلیمون، پس روی یکی از منو‌های بالا کلیک کن و این موضوع بین
+                    خودمون بمونه
                     <br />
                     <br />
-                    پیدا شدید؟
+                    ارادتمند
+                    <br />
+                    گازار
                   </p>
                 </div>
               </>
